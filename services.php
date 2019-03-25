4e05ccdeaea9d1610d288dc27fcc9e508ee2d1d0
DenLahpai <den.lahpai@icloud.com>
2019-03-25 16:21:41 +0630

    fafds

--- services.php
+++ services.php
@@ -1,3 +1,25 @@
 <?php
 require_once "functions.php";
 ?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <?php
+    $page_title = "Services";
+    include "includes/head.html";
+    ?>
+    <body>
+        <!-- content -->
+        <div class="content">
+            <?php
+            $header = "Services";
+            include "includes/header.html";
+            include "includes/nav.html";
+            ?>
+            <main>
+                
+            </main>
+        </div>
+        <!-- end of content -->
+        <?php include "includes/footer.html"; ?>
+    </body>
+</html>
