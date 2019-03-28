944d632dd1375703fa07d45024e3cfdb68f0bc47
Ja Dim Zahkung <dim@Jas-MacBook-Air.local>
2019-03-28 23:05:23 +0630

    starting service_FL.php

---
+++ service_FL.php
@@ -0,0 +1,34 @@
+<?php
+require_once "functions.php";
+?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <?php
+    $page_title = "Service Flight";
+    include "includes/head.html";
+    ?>
+    <body>
+        <!-- content -->
+        <div class="content">
+            <?php
+            $header = "New Serivce: Flight";
+            include "includes/header.html";
+            include "includes/nav.html";
+            ?>
+            <main>
+                <!-- flight form -->
+                <div class="flight form">
+                    <form action="#" method="post">
+                        <ul>
+                            <li>
+                                
+                            </li>
+                        </ul>
+                    </form>
+                </div>
+                <!-- end of flight form -->
+            </main>
+        </div>
+        <!-- end of content -->
+    </body>
+</html>
