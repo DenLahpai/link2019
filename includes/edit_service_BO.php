7200b17e4c5f2940039b8efc40cc90247490e51b
DenLahpai <den.lahpai@icloud.com>
2019-06-05 23:47:30 +0630

    working on edit_service_BO

--- includes/edit_service_BO.php
+++ includes/edit_service_BO.php
@@ -23,6 +23,36 @@
                 Service:
                 <input type="text" name="Service" id="Service" value="<? echo $row_Services->Service; ?>">
             </li>
+            <li>
+                Max pax:
+                <input type="number" name="MaxPax" id="MaxPax" value="<? echo $row_Services->MaxPax; ?>">
+            </li>
+            <li>
+                Valid From:
+                <input type="date" name="StartDate" id="StartDate" value="<? echo $row_Services->StartDate; ?>"onchange="compareDates('StartDate', 'EndDate');" onchange="compareDates('StartDate', 'EndDate');" required>
+                &nbsp;
+                Valid Until:
+                <input type="date" name="EndDate" id="EndDate" value="<? echo $row_Services->EndDate?>" onchange="compareDates('StartDate', 'EndDate');" required>
+            </li>
+            <li class="bold">
+                Cost
+            </li>
+            <li>
+                USD:
+                <input type="number" step="0.01" name="Cost1_USD" id="Cost1_MMK" value="<? echo $row_Services->Cost1_USD; ?>">
+                MMK:
+                <input type="number" name="Cost1_MMK" id="Cost1_MMK" value="<? echo $row_Services->Cost1_MMK; ?>">
+            </li>
+            <li class="error">
+                <?php
+                if (!empty($error)) {
+                    echo $error;
+                }
+                ?>
+            </li>
+            <li>
+                <button type="button" name="buttonSubmit" id="buttonSubmit" onclick="checkThreeFields('SupplierId', 'Service', 'StartDate');">Submit</button>
+            </li>
         </ul>
     </form>
 </div>
